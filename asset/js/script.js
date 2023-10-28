6c0b55ecf465aa1c4ecf6139f9046ce595f8381d
bhatnagardivy1996 <divy.bhatnagar.nds@gmail.com>
2023-10-28 18:14:21 +0530

    New Assets added to site

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(() => {
     if($(window).width() < 768) {
         $(".carousel-item #image1").attr("src", "asset/image/Coming Soon page-mobile.png");
+        $(".carousel-item #image1").removeClass("object-fit-cover");
+
+        $(".carousel-item #image2").attr("src", "asset/image/coming soon page 2-mobile.png");
+        $(".carousel-item #image2").removeClass("object-fit-cover");
+
+        $(".carousel-item #image3").attr("src", "asset/image/coming soon page 3- mobile.png");
+        $(".carousel-item #image3").removeClass("object-fit-cover");
     }
 });
